f6a3c82c58cb0b897512f95a7a1ea9ffcc07a6b9
AZEEZ RIDWAN <azeridwan10@gmail.com>
2026-05-20 03:39:32 +0000

    `Removed logging configuration from settings.py`

--- src/core/settings.py
+++ src/core/settings.py
@@ -184,31 +184,6 @@ JAZZMIN_UI_TWEAKS = {
 # Logging
 # ---------------------------------------------------------------------------
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
-            'style': '{',
-        },
-    },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
-        },
-        'file': {
-            'class': 'logging.FileHandler',
-            'filename': BASE_DIR / 'logs' / 'django.log',
-            'formatter': 'verbose',
-        },
-    },
-    'root': {
-        'handlers': ['console', 'file'],
-        'level': 'INFO',
-    },
-}
 
 # ---------------------------------------------------------------------------
 # Sécurité (production uniquement)
